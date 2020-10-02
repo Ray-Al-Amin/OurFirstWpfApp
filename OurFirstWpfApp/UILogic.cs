0e2cdfe89f119b4d766fadda8e5dce3429e11d0e
Raiman Al-Amin <wi19b069@technikum-wien.at>
2020-10-02 10:24:52 +0200

    k

---
+++ OurFirstWpfApp/UILogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurFirstWpfApp
+{
+    public class UILogic
+    {
+        public string Firstname { get; set; }
+        public UILogic()
+        {
+            Firstname = "Ray";
+        }
+    }
+}
